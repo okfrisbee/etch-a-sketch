27482d2f28d4d2194ecf5e9772a6d5fd0ba07938
Fristine Sok <166497861+okfrisbee@users.noreply.github.com>
2025-03-07 17:54:12 -0800

    Make grid cells evenly spaced

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
 function createGrid(size) {
     const numOfCells = size * size;
     const container = document.querySelector(".container");
+    const width = parseInt(getComputedStyle(container).width)/size;
     for (let i = 0; i < numOfCells; i++) {
         const cell = document.createElement("div");
+        cell.classList.toggle("cell");
+        cell.style.width = width + "px";
+        cell.style.height = width + "px";
         container.appendChild(cell);
     }
 }
