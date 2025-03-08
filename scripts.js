79881633fc35b775ab298626e26fc05b64a1ba87
Fristine Sok <166497861+okfrisbee@users.noreply.github.com>
2025-03-07 19:50:15 -0800

    Add hover effect

--- scripts.js
+++ scripts.js
@@ -1,14 +1,19 @@
 function createGrid(size) {
     const numOfCells = size * size;
-    const container = document.querySelector(".container");
     const width = parseInt(getComputedStyle(container).width)/size;
     for (let i = 0; i < numOfCells; i++) {
         const cell = document.createElement("div");
-        cell.classList.toggle("cell");
+        cell.style.border = "1px solid";
         cell.style.width = width + "px";
         cell.style.height = width + "px";
         container.appendChild(cell);
     }
 }
+
+const container = document.querySelector(".container");
+container.addEventListener("mouseover", (e) => {
+    const targetCell = e.target;
+    targetCell.classList.add("hover");
+});
 
 createGrid(16);
