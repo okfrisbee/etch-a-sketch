fff905714fb2d4c9db4a5d7e35d5b68faf53718a
Fristine Sok <166497861+okfrisbee@users.noreply.github.com>
2025-03-07 20:12:54 -0800

    Add button to create new grid

--- scripts.js
+++ scripts.js
@@ -10,6 +10,18 @@ function createGrid(size) {
     }
 }
 
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    let size = +prompt("Enter the number of squares per side for the new grid (1-100).");
+    while (size < 1 || size > 100) {
+        size = +prompt("Invalid number. Choose a number between 1-100.");
+    }
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    createGrid(size);
+});
+
 const container = document.querySelector(".container");
 container.addEventListener("mouseover", (e) => {
     const targetCell = e.target;
